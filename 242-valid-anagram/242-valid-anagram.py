ac4af0d2de0ea8b8e2b844bc44139d04f0565ea6
hosna <36189572+hosnaa@users.noreply.github.com>
2022-08-11 18:01:09 +0200

    Time: 2114 ms (5.00%), Space: 14.7 MB (25.28%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -4,7 +4,8 @@ class Solution:
         for j in t:
             if j not in list_s:
                 return False
-            _ = list_s.pop(list_s.index(j)) # what's difference between pop() and remove()
+            # _ = list_s.pop(list_s.index(j)) # what's difference between pop() and remove()
+            list_s.remove(j)
 
         if list_s:
             return False
