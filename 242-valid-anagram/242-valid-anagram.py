6008b7c8efd4b6d35199271c047c1670b7b13015
hosna <36189572+hosnaa@users.noreply.github.com>
2022-08-11 17:59:56 +0200

    Time: 2780 ms (5.00%), Space: 14.7 MB (25.28%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        list_s = list(s)
+        for j in t:
+            if j not in list_s:
+                return False
+            _ = list_s.pop(list_s.index(j)) # what's difference between pop() and remove()
+
+        if list_s:
+            return False
+        return True
+        
